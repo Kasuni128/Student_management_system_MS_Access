1360e5073ac2de68b690fc1dae42af2689bd05a4
Kasuni Piyumali Waththage <kasuniwaththage128@gmail.com>
2021-09-20 13:40:03 +0530

    updated

---
+++ IT18259476/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿
+namespace IT18259476
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.courseIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.durationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseFeeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coursesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.collegeDataSet = new IT18259476.collegeDataSet();
+            this.coursesTableAdapter = new IT18259476.collegeDataSetTableAdapters.CoursesTableAdapter();
+            this.orderTableAdapter1 = new IT18259476.collegeDataSetTableAdapters.OrderTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coursesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.coursesBindingSource, "CourseID", true));
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.coursesBindingSource, "CourseName", true));
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.coursesBindingSource, "Duration", true));
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.coursesBindingSource, "CourseFee", true));
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.courseIDDataGridViewTextBoxColumn,
+            this.courseNameDataGridViewTextBoxColumn,
+            this.durationDataGridViewTextBoxColumn,
+            this.courseFeeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.coursesBindingSource;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView2, "dataGridView2");
+            this.dataGridView2.Name = "dataGridView2";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // courseIDDataGridViewTextBoxColumn
+            // 
+            this.courseIDDataGridViewTextBoxColumn.DataPropertyName = "CourseID";
+            resources.ApplyResources(this.courseIDDataGridViewTextBoxColumn, "courseIDDataGridViewTextBoxColumn");
+            this.courseIDDataGridViewTextBoxColumn.Name = "courseIDDataGridViewTextBoxColumn";
+            // 
+            // courseNameDataGridViewTextBoxColumn
+            // 
+            this.courseNameDataGridViewTextBoxColumn.DataPropertyName = "CourseName";
+            resources.ApplyResources(this.courseNameDataGridViewTextBoxColumn, "courseNameDataGridViewTextBoxColumn");
+            this.courseNameDataGridViewTextBoxColumn.Name = "courseNameDataGridViewTextBoxColumn";
+            // 
+            // durationDataGridViewTextBoxColumn
+            // 
+            this.durationDataGridViewTextBoxColumn.DataPropertyName = "Duration";
+            resources.ApplyResources(this.durationDataGridViewTextBoxColumn, "durationDataGridViewTextBoxColumn");
+            this.durationDataGridViewTextBoxColumn.Name = "durationDataGridViewTextBoxColumn";
+            // 
+            // courseFeeDataGridViewTextBoxColumn
+            // 
+            this.courseFeeDataGridViewTextBoxColumn.DataPropertyName = "CourseFee";
+            resources.ApplyResources(this.courseFeeDataGridViewTextBoxColumn, "courseFeeDataGridViewTextBoxColumn");
+            this.courseFeeDataGridViewTextBoxColumn.Name = "courseFeeDataGridViewTextBoxColumn";
+            // 
+            // coursesBindingSource
+            // 
+            this.coursesBindingSource.DataMember = "Courses";
+            this.coursesBindingSource.DataSource = this.collegeDataSet;
+            // 
+            // collegeDataSet
+            // 
+            this.collegeDataSet.DataSetName = "collegeDataSet";
+            this.collegeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // coursesTableAdapter
+            // 
+            this.coursesTableAdapter.ClearBeforeFill = true;
+            // 
+            // orderTableAdapter1
+            // 
+            this.orderTableAdapter1.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coursesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private collegeDataSet collegeDataSet;
+        private System.Windows.Forms.BindingSource coursesBindingSource;
+        private collegeDataSetTableAdapters.CoursesTableAdapter coursesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn durationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseFeeDataGridViewTextBoxColumn;
+        private collegeDataSetTableAdapters.OrderTableAdapter orderTableAdapter1;
+    }
+}
+
---
+++ IT18259476/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+using System.Globalization;
+using System.Threading;
+
+namespace IT18259476
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(CultureInfo cl)
+        {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = cl;
+            Thread.CurrentThread.CurrentUICulture = cl;
+
+            ResourceManager rmg = new ResourceManager("IT18259476.Form1", System.Reflection.Assembly.GetExecutingAssembly());
+
+            this.Text = this.Text + "-" + DateTime.Now.ToLongDateString();
+            button1.Text = rmg.GetString("button1.Text");
+            button2.Text = rmg.GetString("button2.Text");
+            button3.Text = rmg.GetString("button3.Text");
+            button4.Text = rmg.GetString("button4.Text");
+            button5.Text = rmg.GetString("button5.Text");
+            button6.Text = rmg.GetString("button6.Text");
+            button7.Text = rmg.GetString("button7.Text");
+            button8.Text = rmg.GetString("button8.Text");
+
+            label1.Text = rmg.GetString("label1.Text");
+            label2.Text = rmg.GetString("label2.Text");
+            label3.Text = rmg.GetString("label3.Text");
+            label4.Text = rmg.GetString("label4.Text");
+            label5.Text = rmg.GetString("label5.Text");
+            label6.Text = rmg.GetString("label6.Text");
+            label7.Text = rmg.GetString("label7.Text");
+
+        }
+
+        DataSet order_ds;
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'collegeDataSet.Courses' table. You can move, or remove it, as needed.
+            this.coursesTableAdapter.Fill(this.collegeDataSet.Courses);
+            pictureBox1.DataBindings.Add("ImageLocation", coursesBindingSource, "Image", false, DataSourceUpdateMode.Never);
+            order_ds = new DataSet();
+            DataTable ordertb = new DataTable("Order");
+            DataColumn c1 = new DataColumn("CourseID", typeof(int));
+            DataColumn c2 = new DataColumn("Duration", typeof(double));
+            DataColumn c3 = new DataColumn("CourseFee", typeof(double));
+            DataColumn c4 = new DataColumn("StudentName", typeof(string));
+            DataColumn c5 = new DataColumn("StudentID", typeof(int));
+            DataColumn c6 = new DataColumn("Total", typeof(double));
+
+            
+            c6.Expression = "Duration*CourseFee";
+
+            ordertb.Columns.Add(c1);
+            ordertb.Columns.Add(c2);
+            ordertb.Columns.Add(c3);
+            ordertb.Columns.Add(c4);
+            ordertb.Columns.Add(c5);
+            ordertb.Columns.Add(c6);
+
+            ordertb.PrimaryKey = new DataColumn[] { c1 };
+            order_ds.Tables.Add(ordertb);
+            dataGridView2.DataSource = order_ds.Tables[0];
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int id = (int)coursesTableAdapter.getMaxID();
+            textBox1.Text = "" + (id + 1);
+
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            coursesTableAdapter.Insert(
+                int.Parse(textBox1.Text),
+                textBox2.Text,
+                double.Parse(textBox3.Text),
+                double.Parse(textBox4.Text),
+                pictureBox1.ImageLocation);
+
+            MessageBox.Show("New Course Added Sucessfully");
+            this.coursesTableAdapter.Fill(this.collegeDataSet.Courses);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            coursesTableAdapter.Delete(int.Parse(textBox1.Text));
+            MessageBox.Show("Course Removed Sucessfully");
+            this.coursesTableAdapter.Fill(this.collegeDataSet.Courses);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            coursesTableAdapter.Update(
+               textBox2.Text,
+               decimal.Parse(textBox3.Text),
+               decimal.Parse(textBox4.Text),
+               pictureBox1.ImageLocation,
+               int.Parse(textBox1.Text));
+
+            MessageBox.Show("Course Updated Sucessfully");
+            this.coursesTableAdapter.Fill(this.collegeDataSet.Courses);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataRow r = order_ds.Tables[0].NewRow();
+           
+
+            if(textBox1.Text==""||textBox3.Text==""|| textBox4.Text=="")
+            {
+                MessageBox.Show("You Must Select Course Row");
+            }
+            else if(textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("You Must Enter Student ID and Student Name");
+            }
+            else
+            {
+                r[0] = textBox1.Text;
+                r[1] = double.Parse(textBox3.Text);
+                r[2] = double.Parse(textBox4.Text);
+                r[3] = textBox5.Text;
+                r[4] = textBox6.Text;
+                order_ds.Tables[0].Rows.Add(r);
+                MessageBox.Show("Entrolled Sucessfully");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DataRow r = order_ds.Tables[0].Rows.Find(
+                dataGridView2.SelectedRows[0].Cells[0].Value
+                );
+            order_ds.Tables[0].Rows.Remove(r);
+            MessageBox.Show("UnEntrolled Sucessfully");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            String stuDes = "";
+
+            foreach(DataRow r in order_ds.Tables[0].Rows)
+            {
+                stuDes = stuDes + "Student Name:" + r[3] + "," + "Studen Id:" + r[4] + "," + "Course ID:" + r[0];
+                total = total + (double)r[5];
+            }
+            label8.Text = "" + total;
+            orderTableAdapter1.Insert(
+                textBox5.Text,
+                int.Parse(textBox6.Text),
+                stuDes,
+                DateTime.Now,
+                total
+                );
+            MessageBox.Show("Confirmed Sucessfully");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            order_ds.Tables[0].Rows.Clear();
+            label8.Text = "";
+
+        }
+    }
+}
